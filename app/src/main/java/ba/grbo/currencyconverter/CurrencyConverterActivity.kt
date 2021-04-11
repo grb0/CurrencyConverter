dc87d9c31e3060b12441b6a529bbd8d0294a9b32
Grbo <grbo@pop-os.localdomain>
2021-04-11 06:15:31 +0200

    Init commit

---
+++ app/src/main/java/ba/grbo/currencyconverter/CurrencyConverterActivity.kt
@@ -0,0 +1,11 @@
+package ba.grbo.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrencyConverterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency_converter)
+    }
+}
