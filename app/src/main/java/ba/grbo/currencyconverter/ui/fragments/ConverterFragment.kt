4242dc64671332041e0402470a11b907bd43d7f9
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-04-14 21:07:18 +0200

    Add Card, Divider and Auto-scalable text to dropdown currency chooser

--- app/src/main/java/ba/grbo/currencyconverter/ui/fragments/ConverterFragment.kt
+++ app/src/main/java/ba/grbo/currencyconverter/ui/fragments/ConverterFragment.kt
@@ -1,16 +1,22 @@
 package ba.grbo.currencyconverter.ui.fragments
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import ba.grbo.currencyconverter.R
 import ba.grbo.currencyconverter.data.models.CurrencyName
 import ba.grbo.currencyconverter.databinding.FragmentConverterBinding
 import ba.grbo.currencyconverter.ui.adapters.CountryAdapter
 import ba.grbo.currencyconverter.ui.viewmodels.ConverterViewModel
+import ba.grbo.currencyconverter.util.getColorFromAttr
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -29,35 +35,38 @@ class ConverterFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
         }
         val adapter = CountryAdapter(viewLifecycleOwner, CurrencyName.BOTH)
+        binding.fromCurrencyChooser.currencies.addItemDecoration(DividerItemDecoration(
+                requireContext(), DividerItemDecoration.VERTICAL
+        ))
         binding.fromCurrencyChooser.currencies.adapter = adapter
         viewModel.countries.onEach { adapter.submitList(it) }
                 .launchIn(lifecycleScope) // just for testing, needs to be done differently
 
-//        binding.dropdownCurrencyChooser.action.setOnClickListener {
-//            if (!binding.dropdownCurrencyChooser.countryLayout.isSelected) {
-//                binding.dropdownCurrencyChooser.action.setImageResource(R.drawable.ic_collapse)
-//                binding.dropdownCurrencyChooser.action.imageTintList = ColorStateList.valueOf(
-//                        ContextCompat.getColor(requireContext(), R.color.purple_500)
-//                )
-//                binding.dropdownCurrencyChooser.countryLayout.isSelected = true
-//            } else {
-//                binding.dropdownCurrencyChooser.action.setImageResource(R.drawable.ic_expand)
-//                binding.dropdownCurrencyChooser.action.imageTintList = ColorStateList.valueOf(
-//                        requireContext().getColorFromAttr(R.attr.colorControlNormal)
-//                )
-//                binding.dropdownCurrencyChooser.countryLayout.isSelected = false
-//            }
-//        }
-//
-//        binding.dropdownCurrencyChooser.countryLayout.setOnClickListener {
-//            it as LinearLayout
-//            if (it.isSelected) {
-//                binding.dropdownCurrencyChooser.action.setImageResource(R.drawable.ic_expand)
-//            } else {
-//                binding.dropdownCurrencyChooser.action.setImageResource(R.drawable.ic_collapse)
-//            }
-//            it.isSelected = !it.isSelected
-//        }
+        binding.fromCurrencyChooser.dropdownAction.setOnClickListener {
+            if (!binding.fromCurrencyChooser.currencyLayout.isSelected) {
+                binding.fromCurrencyChooser.dropdownAction.setImageResource(R.drawable.ic_collapse)
+                binding.fromCurrencyChooser.dropdownAction.imageTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(requireContext(), R.color.purple_500)
+                )
+                binding.fromCurrencyChooser.currencyLayout.isSelected = true
+            } else {
+                binding.fromCurrencyChooser.dropdownAction.setImageResource(R.drawable.ic_expand)
+                binding.fromCurrencyChooser.dropdownAction.imageTintList = ColorStateList.valueOf(
+                        requireContext().getColorFromAttr(R.attr.colorControlNormal)
+                )
+                binding.fromCurrencyChooser.currencyLayout.isSelected = false
+            }
+        }
+
+        binding.fromCurrencyChooser.currencyLayout.setOnClickListener {
+            it as LinearLayout
+            if (it.isSelected) {
+                binding.fromCurrencyChooser.dropdownAction.setImageResource(R.drawable.ic_expand)
+            } else {
+                binding.fromCurrencyChooser.dropdownAction.setImageResource(R.drawable.ic_collapse)
+            }
+            it.isSelected = !it.isSelected
+        }
 
         return binding.root
     }
