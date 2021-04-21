5b4f4347a46a22630ad7c13166a38e45aef6e42c
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-04-21 10:03:22 +0200

    Refactor code

--- app/src/main/java/ba/grbo/currencyconverter/ui/activities/CurrencyConverterActivity.kt
+++ app/src/main/java/ba/grbo/currencyconverter/ui/activities/CurrencyConverterActivity.kt
@@ -3,33 +3,27 @@ package ba.grbo.currencyconverter.ui.activities
 import android.graphics.Point
 import android.os.Bundle
 import android.view.MotionEvent
-import androidx.activity.viewModels
-import androidx.annotation.IdRes
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import ba.grbo.currencyconverter.R
 import ba.grbo.currencyconverter.databinding.ActivityCurrencyConverterBinding
-import ba.grbo.currencyconverter.ui.viewmodels.CurrencyConverterViewModel
-import ba.grbo.currencyconverter.util.collectWhenStarted
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CurrencyConverterActivity : AppCompatActivity() {
-    private val viewModel: CurrencyConverterViewModel by viewModels()
     private lateinit var binding: ActivityCurrencyConverterBinding
-    private lateinit var navController: NavController
     var onScreenTouched: ((event: MotionEvent) -> Pair<Boolean, Point>)? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_CurrencyConverter)
         super.onCreate(savedInstanceState)
-        collectFlows()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_currency_converter)
-        setUpBottomNavigation()
-        setUpNavController()
+
+        setUp()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
@@ -37,35 +31,20 @@ class CurrencyConverterActivity : AppCompatActivity() {
         return super.dispatchTouchEvent(ev)
     }
 
-    private fun setUpBottomNavigation() {
-        binding.bottomNavigation.setOnNavigationItemSelectedListener {
-            viewModel.onBottomNavigationClicked(it.itemId)
-        }
-
-        // Set an empty lambda to avoid calling the listener above
-        binding.bottomNavigation.setOnNavigationItemReselectedListener { }
-    }
-
-    private fun setUpNavController() {
+    private fun setUp() {
         val fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        navController = (fragment as NavHostFragment).navController.apply {
-            addOnDestinationChangedListener { _, destination, _ ->
-                viewModel.onDestinationChanged(destination.id)
-            }
-        }
-    }
+        val navController = (fragment as NavHostFragment).navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_converter,
+                R.id.navigation_history,
+                R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navController)
 
-    private fun collectFlows() {
-        collectWhenStarted(viewModel.toFragment, ::navigateToDestination)
-        collectWhenStarted(viewModel.actionBarTitle, ::changeActionBarTitle)
-    }
-
-    private fun navigateToDestination(@IdRes destinationId: Int) {
-        navController.popBackStack()
-        navController.navigate(destinationId)
-    }
-
-    private fun changeActionBarTitle(@StringRes title: Int) {
-        supportActionBar?.title = getString(title)
+        // Setting empty one, to avoid fragment reinstantiating
+        binding.bottomNavigation.setOnNavigationItemReselectedListener { }
     }
 }
